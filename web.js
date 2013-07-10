8b78907d958b8b0c92acb6624ca03e07873c6892
Shaun Harding <shwanky@gmail.com>
2013-07-09 20:47:07 -0700

    blah

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  //response.send('Hello World2!');
+  var contents = fs.readFileSync(__dirname + '/index.html');
+  response.send(contents.toString());
 });
 
 var port = process.env.PORT || 5000;
